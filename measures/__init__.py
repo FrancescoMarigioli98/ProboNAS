fae9498afb5acc722badf105d22d71f95245f206
FrancescoMarigioli98 <125659687+FrancescoMarigioli98@users.noreply.github.com>
2023-06-24 19:09:43 +0200

    Utils for the training-free measures

---
+++ measures/__init__.py
@@ -0,0 +1,42 @@
+
+available_measures = []
+_measure_impls = {}
+
+
+def measure(name, bn=True, copy_net=True, force_clean=True, **impl_args):
+    def make_impl(func):
+        def measure_impl(net_orig, device, *args, **kwargs):
+            if copy_net:
+                net = net_orig.get_prunable_copy(bn=bn).to(device)
+            else:
+                net = net_orig
+            ret = func(net, device, *args, **kwargs, **impl_args)
+            if copy_net and force_clean:
+                import gc
+                import torch
+                del net
+                torch.cuda.empty_cache()
+                gc.collect()
+            return ret
+
+        global _measure_impls
+        if name in _measure_impls:
+            raise KeyError(f'Duplicated measure! {name}')
+        available_measures.append(name)
+        _measure_impls[name] = measure_impl
+        return func
+    return make_impl
+
+
+def calc_measure(name, net, device, *args, **kwargs):
+    return _measure_impls[name](net, device, *args, **kwargs)
+
+
+def load_all():
+    from . import logsynflow
+    from . import naswot
+
+
+
+
+load_all()
